da9df82cf8a58c7486678700fa2abeb0d6b24ce1
Kevin Kolk <kkolk@frostbyte.us>
2019-01-11 16:37:26 -0500

    Completed case study layout.

--- single-casestudies.php
+++ single-casestudies.php
@@ -9,46 +9,30 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
+
+<div class="header_art_before"></div>
+<div class="header_art" style="background-image:url(/wp-content/uploads/2018/08/header_services_taller.jpg);">
+<?php the_title( '<h1 class="header-title">', '</h1>' ); ?>
+</div>
+
+<div id="content-area-case-study">
 	<main id="main" class="site-main" role="main">
+
+
+
 		<?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();
-
+		
 			// Include the single post content template.
-			get_template_part( 'template-parts/content', 'single' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
-
-			if ( is_singular( 'attachment' ) ) {
-				// Parent post navigation.
-				the_post_navigation( array(
-					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
-				) );
-			} elseif ( is_singular( 'post' ) ) {
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
-			}
+			get_template_part( 'template-parts/content-casestudy', 'single' );
 
 			// End of the loop.
 		endwhile;
 		?>
 
-	</main><!-- .site-main -->
 
-	<?php get_sidebar( 'content-bottom' ); ?>
-
+	</main>
 </div><!-- .content-area -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- template-parts/content-casestudy.php
+++ template-parts/content-casestudy.php
@@ -1,29 +1,16 @@
 <?php
 /**
- * The template part for displaying content
+ * The template part for displaying single posts
+ * applies to blog and white papers posts
  *
  * @package WordPress
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
  */
 ?>
-<?php 
-$pageID = get_option('page_for_posts');
-$image = wp_get_attachment_image_src( get_post_thumbnail_id( $pageID ), 'single-post-thumbnail' );$imageURL = $image[0];
-$title = get_the_title($pageID);
-?>
-<div class="header_art_before"></div>
-<div class="header_art" style="background-image:url(<?php echo htmlspecialchars($imageURL); ?>);">
-<h1 class="header-title"><?php the_title(); ?></h1>
-</div>
-
-<div>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php the_content(); ?>
-
-	
 
 	<div class="entry-content">
 		<?php
@@ -45,7 +32,6 @@ $title = get_the_title($pageID);
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		
 		<?php
 			edit_post_link(
 				sprintf(
@@ -56,6 +42,8 @@ $title = get_the_title($pageID);
 				'<span class="edit-link">',
 				'</span>'
 			);
+
 		?>
 	</footer><!-- .entry-footer -->
+
 </article><!-- #post-## -->
