13cb84e29c89fd1e5413760df3be25f42db5e922
Kevin Kolk <kkolk@frostbyte.us>
2019-01-11 15:53:16 -0500

    Adjusting.

--- single-casestudies.php
+++ single-casestudies.php
@@ -9,15 +9,15 @@
 
 get_header(); ?>
 
-		
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 		<?php
-
 		// Start the loop.
 		while ( have_posts() ) : the_post();
 
 			// Include the single post content template.
-			get_template_part( 'template-parts/content-casestudy', 'single' );
-		
+			get_template_part( 'template-parts/content', 'single' );
+
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
 				comments_template();
@@ -44,7 +44,11 @@ get_header(); ?>
 		endwhile;
 		?>
 
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
 </div><!-- .content-area -->
 
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
