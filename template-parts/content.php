4940e71dd1cf241cc7de98ab0df8055d22a3dcb4
Kevin Kolk <kkolk@frostbyte.us>
2019-01-16 18:35:21 -0500

    Updated resource page navigation.

--- template-parts/content.php
+++ template-parts/content.php
@@ -6,21 +6,19 @@
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
  */
+
+  $postCounter = $wp_query->current_post
 ?>
 
+<!-- <div id="boxed_half<?php if (($postCounter % 5) == 0) echo '_wide_box'; ?>"> -->
+<div id="boxed_half">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="blog_image_container">
-		<?php twentysixteen_post_thumbnail(); ?>
+	<div class="image_container">
+	<?php echo '<a href=" ' . get_post_permalink($recent_blog_post) .' ">' ?>
+		<?php the_post_thumbnail(); ?></a>
 	</div>
+	<div class="resource_text_container">
 	<header class="entry-header">
-		<?php 
-			$fname = get_the_author_meta('first_name');
-			$lname = get_the_author_meta('last_name');
-			echo '<span class="date">' . get_the_date() . '</span>';
-			echo '<span class="author">by ' . trim( "$fname $lname" ) . '</span>';
-			echo the_category();
-
-		?>
 		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
 			<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
 		<?php endif; ?>
@@ -28,16 +26,11 @@
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 	</header><!-- .entry-header -->
 
-
-	<?php twentysixteen_excerpt(); ?>
-
-	
-
 	<div class="entry-content">
 		<?php 
-			$content = get_the_content(); 
-			echo '<p>' . mb_strimwidth($content, 0, 600, '...') . '</p>';
-	
+			$content = strip_tags(get_the_excerpt()); 
+			echo '<p>' . mb_strimwidth($content, 0, 140, '...') . '</p>';
+		
 			/* translators: %s: Name of current post 
 			the_content( sprintf(
 				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
@@ -61,19 +54,9 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
+	
+			</div> <!-- .resource_text_container -->
+			
 </article><!-- #post-## -->
+<div class="read_more"><?php echo '<a href=" ' . get_post_permalink($recent_blog_post) .' "> read now </a>';?></div>
+</div> <!-- boxed_half -->
