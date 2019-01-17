cad3613cf75875812936a00572a2bf8a4ed5a0c9
Kevin Kolk <kkolk@frostbyte.us>
2019-01-17 11:41:33 -0500

    Replacing phone with relative URL.

--- contactform.php
+++ contactform.php
@@ -5,7 +5,7 @@
 			<p style="text-align: center;">We know every project is unique.<br/>
 					Let us know how we can help.</p>
 			<h2>800-574-4518</h2>
-			<p style="text-align: center;"><img style="margin-left: -25px;" src="http://localhost:8080/wp-content/uploads/2019/01/smartphone.png" width="100px"></p>
+			<p style="text-align: center;"><img style="margin-left: -25px;" src="/wp-content/uploads/2019/01/smartphone.png" width="100px"></p>
 	</div>
 	<div>
 		<?php echo do_shortcode('[contact-form-7 id="312" title="Contact Us"]'); ?> 
