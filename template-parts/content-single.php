6b4c81dbc95f6bae7f73d97d961546896709717b
Kevin Kolk <kkolk@frostbyte.us>
2019-01-17 11:13:11 -0500

    Adding br to contact us form.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -59,8 +59,8 @@
 	<div id="boxed_content">
 	<div id="boxed_half_text">
 			<h2>Contact Us</h2>
-			<p style="text-align: center;">We know every project is unique.
-					Let us know how we can help.
+			<p style="text-align: center;">We know every project is unique.<br/>
+					Let us know how we can help.</p>
 			<h2>800-574-4518</h2>
 			<p style="text-align: center;"><img style="margin-left: -25px;" src="http://localhost:8080/wp-content/uploads/2019/01/smartphone.png" width="100px"></p>
 	</div>
